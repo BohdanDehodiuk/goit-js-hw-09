606ea23c131b23e7988aabb6273ff7d30f89a83c
BohdanDehodiuk <86683356+BohdanDehodiuk@users.noreply.github.com>
2021-11-29 21:53:27 +0200

    make 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,44 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let body = document.body;
+const PROMPT_DELAY = 1000;
+let promptCounter = 0;
+let intervalId = null;
+let isActive = false;
+
+startBtn.addEventListener('click', startChangeColor);
+
+stopBtn.addEventListener('click', stopChangeColor);
+
+function startChangeColor() {
+  if (isActive) {
+    return;
+  }
+
+  const makeDisable = stopBtn.hasAttribute('disabled');
+  if (makeDisable) {
+    stopBtn.removeAttribute('disabled');
+  }
+
+  startBtn.setAttribute('disabled', 'disabled');
+  isActive = true;
+
+  intervalId = setInterval(() => {
+    body.style.background = getRandomHexColor();
+    promptCounter += 1;
+  }, PROMPT_DELAY);
+}
+
+function stopChangeColor() {
+  if (isActive) {
+    clearInterval(intervalId);
+    stopBtn.setAttribute('disabled', 'disabled');
+    startBtn.removeAttribute('disabled');
+    isActive = false;
+    return;
+  }
+}
